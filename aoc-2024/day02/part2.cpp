239cb47a75f1f03830fb233ff0a37a17f7ec65ac
Julio Vivas <jcvmlego@gmail.com>
2024-12-12 01:40:12 -0600

    Day02 2024 Solved both

---
+++ aoc-2024/day02/part2.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO(string s) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+}
+
+bool isValidSequence(const vector<int>& nums) {
+    if(nums.size() < 2) return true;
+
+    bool isIncreasing = nums[1] > nums[0];
+    for(size_t i = 1; i < nums.size(); ++i) {
+        int diff = nums[i] - nums[i - 1];
+        if(isIncreasing) {
+            if(diff <= 0 || diff > 3) return false;
+        } else {
+            if(diff >= 0 || diff < -3) return false;
+        }
+    }
+    return true;
+}
+
+bool isValidWithOneRemoval(const vector<int>& nums) {
+    for(size_t i = 0; i < nums.size(); ++i) {
+        vector<int> modifiedNums;
+        for(size_t j = 0; j < nums.size(); ++j) {
+            if(i != j) {
+                modifiedNums.push_back(nums[j]);
+            }
+        }
+        if(isValidSequence(modifiedNums)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    setIO("input");
+
+    vector<vector<int>> reports;
+    string line;
+
+    while(getline(cin, line)) {
+        stringstream ss(line);
+        vector<int> row;
+        int num;
+        while(ss >> num) {
+            row.push_back(num);
+        }
+        reports.push_back(row);
+    }
+
+    int safeReports = 0;
+    for(const auto& report : reports) {
+        if(isValidSequence(report) || isValidWithOneRemoval(report)) {
+            safeReports++;
+        }
+    }
+
+    cout << safeReports << "\n";
+
+    return 0;
+}
